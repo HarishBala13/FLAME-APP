2c25450606404bc79f745448de6de4db5c98d1b6
HarishBala13 <2k19me016@kiot.ac.in>
2023-03-18 23:14:52 +0530

    form email success comiit

--- server.js
+++ server.js
@@ -118,85 +118,106 @@ app.get('/payment',(req,res)=>{
             
              var transporter=nodemailer.createTransport({
                service:'gmail',
-               host: "2k19me070@kiot.ac.in",
+               host: "smtp.gmail.com",
                port: 465,
                secure: true,
                auth:{
-                  user:"2k19me070@kiot.ac.in",
-                  pass:"Jeeva2001@arav"
+                  user:process.env.EMAIL_ID,
+                  pass:process.env.EMAIL_PASSWORD
                }
                });
             
-               var mailoptions={
-               from:'2k19me070@kiot.ac.in',
-               to:email,
-               subject:'FLAME 2k23 - Event Registration',
-               html:`         
-               <h4 style=" font-size: 20px; color: black;">Hi Participant(s),</h4>
-               <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Flame_with_bg-removebg-preview.png" alt="FLAME logo" style=" width: 250px; height: 200px;  border-radius: 50%;">
-               <p style=" text-align: justify; font-size: 20px; color: black;">Thanks for registering the Event in Flame 2k23. Please ensure that you made a payment, otherwise make the payment for confirming the event.</p>
-               <p style=" text-align: justify;  font-size: 20px; color: black;">Regards,<br>FLAME 2k23.</p>
-               <br><br>
-               <a href="https://www.facebook.com/profile.php?id=100090942617082" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Facebook.png" alt="Facebook" title="Facebook"> </a>
-               <a href="https://instagram.com/flame_mechroniz_2k23?igshid=ZDdkNTZiNTM=" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Instagram.png" alt="Instagram" title="Instagram"></a>
-               <a href="https://www.linkedin.com/in/flame-kiot-26aa53269/" target="_blank"><img src="https://healthy-teal-skunk.cyclic.app/assets/images/Linkedin.png" alt="Linkedin" title="Linkedin"></a>
-               `
-            };
-               
-            transporter.sendMail(mailoptions,(err,info)=>{
-            if(err) throw err;
-            else{
-            console.log('email sent : '+info.response);          
-            }
-          });
-             res.render("payment");
-           
-          }
-         }
-         else{
-            if(amount==money){
-               image=qr[i];
-               Participantname=name1;
-               console.log(image);
-               
-               var transporter=nodemailer.createTransport({
-                  service:'gmail',
-                  host: "2k19me070@kiot.ac.in",
-                  port: 465,
-                  secure: true,
-                  auth:{
-                     user:"2k19me070@kiot.ac.in",
-                     pass:"Jeeva2001@arav"
-                  }
+                  await new Promise((resolve,reject)=>{
+                  transporter.verify(function (error,success){
+                     if(error){
+                        console.log(error);
+                        reject(error);
+                     }
+                     else{
+                        console.log("Server is ready to send the Email....Done!!");
+                        resolve(succes);
+                     }
                   });
-               
+                  });   
+
                   var mailoptions={
-                  from:'2k19me070@kiot.ac.in',
-                  to:email,
-                  subject:'FLAME 2k23 - Event Registration',
-                  html:`         
-                  <h4 style=" font-size: 20px; color: black;">Hi Participant(s),</h4>
-                  <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Flame_with_bg-removebg-preview.png" alt="FLAME logo" style=" width: 250px; height: 200px;  border-radius: 50%;">
-                  <p style=" text-align: justify; font-size: 20px; color: black;">Thanks for registering the Event in Flame 2k23. Please ensure that you made a payment, otherwise make the payment for confirming the event.</p>
-                  <p style=" text-align: justify;  font-size: 20px; color: black;">Regards,<br>FLAME 2k23.</p>
-                  <br><br>
-                  <a href="https://www.facebook.com/profile.php?id=100090942617082" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Facebook.png" alt="Facebook" title="Facebook"> </a>
-                  <a href="https://instagram.com/flame_mechroniz_2k23?igshid=ZDdkNTZiNTM=" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Instagram.png" alt="Instagram" title="Instagram"></a>
-                  <a href="https://www.linkedin.com/in/flame-kiot-26aa53269/" target="_blank"><img src="https://healthy-teal-skunk.cyclic.app/assets/images/Linkedin.png" alt="Linkedin" title="Linkedin"></a>
-                  `
-               };
+                     from:process.env.EMAIL_ID,
+                     to:email,
+                     subject:'FLAME 2k23 - Event Registration',
+                     html:`         
+                     <h4 style=" font-size: 20px; color: black;">Hi Participant(s),</h4>
+                     <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Flame_with_bg-removebg-preview.png" alt="FLAME logo" style=" width: 250px; height: 200px;  border-radius: 50%;">
+                     <p style=" text-align: justify; font-size: 20px; color: black;">Thanks for registering the Event in Flame 2k23. Please ensure that you made a payment, otherwise make the payment for confirming the event.</p>
+                     <p style=" text-align: justify;  font-size: 20px; color: black;">Regards,<br>FLAME 2k23.</p>
+                     <br><br>
+                     <a href="https://www.facebook.com/profile.php?id=100090942617082" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Facebook.png" alt="Facebook" title="Facebook"> </a>
+                     <a href="https://instagram.com/flame_mechroniz_2k23?igshid=ZDdkNTZiNTM=" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Instagram.png" alt="Instagram" title="Instagram"></a>
+                     <a href="https://www.linkedin.com/in/flame-kiot-26aa53269/" target="_blank"><img src="https://healthy-teal-skunk.cyclic.app/assets/images/Linkedin.png" alt="Linkedin" title="Linkedin"></a>`
+                        };    
+                        await new Promise((resolve,reject)=>{
+                           transporter.sendMail(mailoptions,(err,info)=>{
+                              if(err) console.log(err);
+                              else  console.log('email sent : '+info.response);          
+                           });
+                     });
+            }
+            res.render("payment");  
+         }
+
+         else{
+               if(amount==money){
+                  image=qr[i];
+                  Participantname=name1;
+                  console.log(image);
+                  var transporter=nodemailer.createTransport({
+                     service:'gmail',
+                     host: "smtp.gmail.com",
+                     port: 465,
+                     secure: true,
+                     auth:{
+                        user:process.env.EMAIL_ID,
+                        pass:process.env.EMAIL_PASSWORD
+                     }
+                     });
                   
-               transporter.sendMail(mailoptions,(err,info)=>{
-               if(err) throw err;
-               else{
-               console.log('email sent : '+info.response);          
-               }
-             });
-               res.render("payment");
-              
+                        await new Promise((resolve,reject)=>{
+                        transporter.verify(function (error,success){
+                           if(error){
+                              console.log(error);
+                              reject(error);
+                           }
+                           else{
+                              console.log("Server is ready to send the Email....Done!!");
+                              resolve(succes);
+                           }
+                        });
+                        });   
+      
+                        var mailoptions={
+                           from:process.env.EMAIL_ID,
+                           to:email,
+                           subject:'FLAME 2k23 - Event Registration',
+                           html:`         
+                           <h4 style=" font-size: 20px; color: black;">Hi Participant(s),</h4>
+                           <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Flame_with_bg-removebg-preview.png" alt="FLAME logo" style=" width: 250px; height: 200px;  border-radius: 50%;">
+                           <p style=" text-align: justify; font-size: 20px; color: black;">Thanks for registering the Event in Flame 2k23. Please ensure that you made a payment, otherwise make the payment for confirming the event.</p>
+                           <p style=" text-align: justify;  font-size: 20px; color: black;">Regards,<br>FLAME 2k23.</p>
+                           <br><br>
+                           <a href="https://www.facebook.com/profile.php?id=100090942617082" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Facebook.png" alt="Facebook" title="Facebook"> </a>
+                           <a href="https://instagram.com/flame_mechroniz_2k23?igshid=ZDdkNTZiNTM=" target="_blank"> <img src="https://healthy-teal-skunk.cyclic.app/assets/images/Instagram.png" alt="Instagram" title="Instagram"></a>
+                           <a href="https://www.linkedin.com/in/flame-kiot-26aa53269/" target="_blank"><img src="https://healthy-teal-skunk.cyclic.app/assets/images/Linkedin.png" alt="Linkedin" title="Linkedin"></a>`
+                              };    
+                              await new Promise((resolve,reject)=>{
+                                 transporter.sendMail(mailoptions,(err,info)=>{
+                                    if(err) console.log(err);
+                                    else  console.log('email sent : '+info.response);          
+                                 });
+                           });
+                  res.render("payment");
+               
+               } 
             } 
          }
-       }   
 });
 
 connectDB().then(()=>{
