f7da27aa1c5023baa622cc206bf4bf2ce369dba2
HarishBala13 <2k19me016@kiot.ac.in>
2023-03-21 10:20:44 +0530

    updated final form

--- server.js
+++ server.js
@@ -68,11 +68,11 @@ app.get('/payment',(req,res)=>{
    const workshop=req.body.workshop;
    var amount=0;
    if(workshop=="yes"){
-      if(techevent=="" && nontechevent==""){
-         amount=member*300;
+      if(techevent!=undefined || nontechevent!=undefined){
+         amount=member*450;
       }
       else{
-         amount=member*450;
+         amount=member*300;
       }
    }
    else{
